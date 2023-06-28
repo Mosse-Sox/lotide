78e15ac9eb18d3e607c3efd8d994797736bf083b
Mosse-Sox <autumngreenie990@gmail.com>
2023-06-28 21:32:11 +0000

    middle returns the middle most element of an array or middle more elements if the array length is even

--- middle.js
+++ middle.js
@@ -1,11 +1,11 @@
-const eqArrays = function(actual, expected) {
+const eqArrays = function (actual, expected) {
   let matching = true;
 
   for (let i = 0; i < actual.length; i++) {
     const actualElement = actual[i];
     const expectedElement = expected[i];
 
-    if (actualElement !== expectedElement || typeof(actualElement) !== typeof(expectedElement)) {
+    if (actualElement !== expectedElement || typeof (actualElement) !== typeof (expectedElement)) {
       matching = false;
       return matching;
     }
@@ -15,8 +15,8 @@ const eqArrays = function(actual, expected) {
 };
 
 
-const assertArraysEqual = function(actual, expected) {
-  
+const assertArraysEqual = function (actual, expected) {
+
   if (!eqArrays(actual, expected)) {
     console.log(`❗️❗️❗️ ${actual} !== ${expected}`);
     return;
@@ -25,14 +25,29 @@ const assertArraysEqual = function(actual, expected) {
   console.log(`🍀🍀🍀 ${actual} === ${expected}`);
 };
 
-const middle = function(array) {
+const middle = function (array) {
   let middleElements = [];
 
-  for(let i = 1; i < array.length - 1; i++) {
-    middleElements.push(array[i]);
+  if (array.length === 0 || array.length === 1 || array.length === 2) {
+    return middleElements;
   }
 
+  if (array.length % 2 === 0) {
+    const indexOne = Math.floor(array.length / 2) - 1;
+    const indexTwo = Math.ceil(array.length / 2);
+    middleElements = [ array[ indexOne ], array[ indexTwo ] ];
+    return middleElements;
+  }
+
+  const middleIndex = Math.floor(array.length / 2);
+  middleElements.push(array[middleIndex]);
   return middleElements;
 }
 
-// assertArraysEqual()
+// test cases
+assertArraysEqual(middle([4,5,6]), [5]);
+assertArraysEqual(middle([4,5,6,7]), [5,6]);
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1,2]), []);
+
