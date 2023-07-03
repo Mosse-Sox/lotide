6f38936baf3790e484aae3234cca36b08bf8b665
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-03 21:45:23 +0000

    drying up some code, assertEqual done

--- assertEqual.js
+++ assertEqual.js
@@ -14,5 +14,7 @@ const assertEqual = function(actual, expected) {
   console.log(`🫥🫥🫥 ${actual} !== ${expected} 🫥🫥🫥`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+module.exports = assertEqual; 
--- head.js
+++ head.js
@@ -1,18 +1,4 @@
-// this function returns a message based on if the values passed to it are equal or not
-const assertEqual = function(actual, expected) {
-  let matching = false;
-
-  if (actual === expected) {
-    matching = true;
-  }
-
-  if (matching === true) {
-    console.log(`🍀🍀🍀 ${actual} === ${expected}`);
-    return;
-  }
-
-  console.log(`❗️❗️❗️ ${actual} !== ${expected}`);
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   if (array.length > 0) {
